0c18d9d150caf016a146753f8c4b2aa3fca72d6b
edgolovin <edgolovin@yandex.ru>
2020-04-23 16:20:40 +0700

    several ways to extract text from html

--- hyperskill/hyperskill_soup.py
+++ hyperskill/hyperskill_soup.py
@@ -1,19 +1,39 @@
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 import string
 
 
+# how to extract all text contents from html
+
 def main():
+    wanted_tags = ['a', 'p', 'ul', 'ol', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    strainer = SoupStrainer(wanted_tags)
+
     with open('docs_python_org.html', encoding='utf-8') as f:
-        soup = BeautifulSoup(f, 'html.parser')
+        # soup = BeautifulSoup(f, 'html.parser')
+        strained_soup = BeautifulSoup(f, 'html.parser', parse_only=strainer)
 
-    body = soup.html.body.children
+    # strainer filters some unuseful tags from html
+    for st in strained_soup.stripped_strings:
+        print(repr(st))
 
-    for child in body:
-        try:
-            text = child.get_text().strip()
-            print(text)
-        except AttributeError:
-            print(f'{type(child)} has no attribute "get_text"')
+    # body = soup.html.body
+
+    # best practice to extract text contents
+    # the problem is, it returns also unnecessary text data e.g. script text
+    # for st in body.stripped_strings:
+    #     print(repr(st))
+
+    # this way is bad, repeatedly returns same strings from different levels of tree
+    # looping body.children is better, but still returns too many empty lines
+
+    # for child in body.descendants:
+    #     try:
+    #         text = child.get_text().strip()
+    #         print(text)
+    #     except AttributeError:
+    #         # print(f'{type(child)} has no attribute "get_text"')
+    #         pass
 
     # with open('stepik_courses.csv', 'a', newline='', encoding='utf-8') as csv_file:
     #     my_writer = csv.writer(csv_file)
